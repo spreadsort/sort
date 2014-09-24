bcc46e72f7d8aab6e106a244f8ba48ea867c4d65
Steven Ross <spreadsort@gmail.com>
2014-09-23 21:51:48 -0400

    Fixing up whitespace

--- tune.pl
+++ tune.pl
@@ -1,9 +1,8 @@
 #!/usr/bin/perl -w
-$usage = "usage: tune.pl [-tune] [-real] [-tune_verify] [-verbose] "
-"[-multiple_iterations] [-large] [-small] [-windows] [fileSize]\n";
-#testing sorting on 40 million elements by default
-#don't test on below 2^22 (4 million) elements as that is the minimum
-#for MAX_SPLITS of 11 to be efficient
+$usage = "usage: tune.pl [-tune] [-real] [-tune_verify] [-verbose] [-multiple_iterations] [-large] [-small] [-windows] [fileSize]\n";
+# testing sorting on 40 million elements by default
+# don't test on below 2^22 (4 million) elements as that is the minimum
+# for MAX_SPLITS of 11 to be efficient
 use File::Compare;
 $defFileSize = 5000000;
 $loopCount = 1;
@@ -19,54 +18,55 @@ $log = "> .tunelog";
 $log2 = "> .tunelog 2>&1";
 $diffopt = "-q";
 $tune = 0;
-#have to change the path for UNIX
+# have to change the path for UNIX
 $prev_path = $ENV{'PATH'}; 
 $ENV{'PATH'} = '.:'.$prev_path;
 
 for (my $ii = 0; $ii < @ARGV; $ii++) {
-	my $currArg = $ARGV[$ii];
-	if ($currArg =~ /^-help$/) {
-	    print STDERR $usage;
-	    exit(0);
-	}
-        #verification roughly doubles the runtime of this script,
-        #but it does make sure that results are correct during tuning
-	#verification always runs during speed comparisons with std::sort
-	if ($currArg =~ /^-tune_verify$/) {
-	    $verifycorrect = 1;
-	#use real times only, don't use weighting and special-case tests
-        #this saves about 5/6 of the script runtime but results are different
-	} elsif ($currArg =~ /^-real$/) {
-	    $realtimes = 1;
-	} elsif ($currArg =~ /^-verbose$/) {
-	    $verbose = 1;
-	#runs until we converge on a precise set of values; defaults off because of runtime
-	} elsif ($currArg =~ /^-multiple_iterations$/) {
-	    $iter_count = 4;
-	} elsif ($currArg =~ /^-debug$/) {
-	    $debug = 1;
-	    $log = "";
-	    $diffopt = "";
-	} elsif ($currArg =~ /^-large$/) {
-	    $defFileSize = 20000000;
-	} elsif ($currArg =~ /^-small$/) {
-	    $defFileSize = 100000;
-	} elsif ($currArg =~ /^-tune$/) {
-	    $tune = 1;
-	} elsif ($currArg =~ /^-windows$/) {
-	    $makename = "..\\..\\".$makename;
-	} elsif ($currArg =~ /^-/) {
-	    print STDERR $usage;
-	    exit(0);
-	} else {
-		$defFileSize = $currArg;
-	}
+        my $currArg = $ARGV[$ii];
+        if ($currArg =~ /^-help$/) {
+            print STDERR $usage;
+            exit(0);
+        }
+        # verification roughly doubles the runtime of this script,
+        # but it does make sure that results are correct during tuning
+        # verification always runs during speed comparisons with std::sort
+        if ($currArg =~ /^-tune_verify$/) {
+            $verifycorrect = 1;
+        # use real times only, don't use weighting and special-case tests
+        # this saves about 5/6 of the script runtime but results are different
+        } elsif ($currArg =~ /^-real$/) {
+            $realtimes = 1;
+        } elsif ($currArg =~ /^-verbose$/) {
+            $verbose = 1;
+        # runs until we converge on a precise set of values
+        # defaults off because of runtime
+        } elsif ($currArg =~ /^-multiple_iterations$/) {
+            $iter_count = 4;
+        } elsif ($currArg =~ /^-debug$/) {
+            $debug = 1;
+            $log = "";
+            $diffopt = "";
+        } elsif ($currArg =~ /^-large$/) {
+            $defFileSize = 20000000;
+        } elsif ($currArg =~ /^-small$/) {
+            $defFileSize = 100000;
+        } elsif ($currArg =~ /^-tune$/) {
+            $tune = 1;
+        } elsif ($currArg =~ /^-windows$/) {
+            $makename = "..\\..\\".$makename;
+        } elsif ($currArg =~ /^-/) {
+            print STDERR $usage;
+            exit(0);
+        } else {
+                $defFileSize = $currArg;
+        }
 }
 $fileSize = $defFileSize;
 
 print STDOUT "Tuning variables for $exename on vectors with $defFileSize elements\n";
 
-#these are reasonable values
+# these are reasonable values
 $max_splits = 11;
 $log_finishing_count = 31;
 $log_min_size = 11;
@@ -75,45 +75,46 @@ $float_log_min_size = 10;
 $float_log_mean_bin_size = 2;
 $float_log_finishing_count = 4;
 
-#this value is a minimum to obtain decent file I/O performance
+# this value is a minimum to obtain decent file I/O performance
 $min_sort_size = 1000;
 $std = "";
 
 print STDOUT "building randomgen\n";
 system("$makename randomgen $log");
-#Tuning to get convergence, maximum of 4 iterations with multiple iterations option set
+# Tuning to get convergence, maximum of 4 iterations with multiple iterations option set
 $changed = 1;
 my $ii = 0;
 if($tune) {
-	for($ii = 0; $changed and $ii < $iter_count; $ii++) {
-		$changed = 0;
-		print STDOUT "Tuning max_splits\n";
-		#TuneVariable(\$max_splits, $log_min_size - $log_mean_bin_size, 17);
-		print STDOUT "Tuning log of the minimum count for recursion\n";
-		TuneVariable(\$log_min_size, $log_mean_bin_size + 1, $max_splits + $log_mean_bin_size);
-		print STDOUT "Tuning log_mean_bin_size\n";
-		TuneVariable(\$log_mean_bin_size, 0, $log_min_size - 1);
-		print STDOUT "Tuning log_finishing_size\n";
-	    TuneVariable(\$log_finishing_count, 1, $log_min_size);
-		#tuning variables for floats
-		$exename = "floatsort";
-	    print STDOUT "Tuning log of the minimum count for recursion for floats\n";
-		TuneVariable(\$float_log_min_size, $float_log_mean_bin_size + 1, $max_splits + $float_log_mean_bin_size);
-		print STDOUT "Tuning float_log_mean_bin_size\n";
-		TuneVariable(\$float_log_mean_bin_size, 0, $float_log_min_size - 1);
+        for($ii = 0; $changed and $ii < $iter_count; $ii++) {
+                $changed = 0;
+                # Tuning max_splits is not recommended.
+                #print STDOUT "Tuning max_splits\n";
+                #TuneVariable(\$max_splits, $log_min_size - $log_mean_bin_size, 17);
+                print STDOUT "Tuning log of the minimum count for recursion\n";
+                TuneVariable(\$log_min_size, $log_mean_bin_size + 1, $max_splits + $log_mean_bin_size);
+                print STDOUT "Tuning log_mean_bin_size\n";
+                TuneVariable(\$log_mean_bin_size, 0, $log_min_size - 1);
+                print STDOUT "Tuning log_finishing_size\n";
+            TuneVariable(\$log_finishing_count, 1, $log_min_size);
+                # tuning variables for floats
+                $exename = "floatsort";
+            print STDOUT "Tuning log of the minimum count for recursion for floats\n";
+                TuneVariable(\$float_log_min_size, $float_log_mean_bin_size + 1, $max_splits + $float_log_mean_bin_size);
+                print STDOUT "Tuning float_log_mean_bin_size\n";
+                TuneVariable(\$float_log_mean_bin_size, 0, $float_log_min_size - 1);
 		print STDOUT "Tuning float_log_finishing_size\n";
 	    TuneVariable(\$float_log_finishing_count, 1, $float_log_min_size);
 		$exename = "spreadsort";
 	}
 
-	#After optimizations for large datasets are complete, see how small of a dataset can be sped up
+	# After optimizations for large datasets are complete, see how small of a dataset can be sped up
 	print STDOUT "Tuning minimum sorting size\n";
 	TuneMinSize();
 	print STDOUT "Writing results\n";
 }
 
-#Doing a final run with final settings to compare sort times
-#also verifying correctness of results
+# Doing a final run with final settings to compare sort times
+# also verifying correctness of results
 $verifycorrect = 1;
 $loopCount = 1;
 $fileSize = $defFileSize;
@@ -135,16 +136,16 @@ PerfTest("Verifying string_sort with indexing functors", "charstringsort");
 PerfTest("Verifying string_sort with all functors", "stringfunctorsort");
 PerfTest("Verifying reverse_string_sort", "reversestringsort");
 PerfTest("Verifying reverse_string_sort with functors", "reversestringfunctorsort");
-#clean up once we finish
+# clean up once we finish
 system("$makename clean $log");
-#WINDOWS
+# WINDOWS
 system("del spread_sort_out.txt $log2");
 system("del standard_sort_out.txt $log2");
 system("del input.txt $log2");
 system("del *.rsp $log2");
 system("del *.manifest $log2");
 system("del time.txt $log2");
-#UNIX
+# UNIX
 system("rm -f time.txt $log2");
 system("rm -f spread_sort_out.txt $log2");
 system("rm -f standard_sort_out.txt $log2");
@@ -222,7 +223,7 @@ sub WriteConstants {
 sub CheckTime {
 	my $sort_time = 0.0;
 	my $time_file = "time.txt";
-	#use the line below on systems that can't overwrite.
+	# use the line below on systems that can't overwrite.
 	#system("rm -f $time_file");
 	system("$exename $loopCount $std > $time_file");
 	unless(open(CODE, $time_file)) {
@@ -237,7 +238,7 @@ sub CheckTime {
 	 }		
 	}
 	close(CODE);
-	#verifying correctness
+	# verifying correctness
 	if(not $std and $verifycorrect) {
 		system("$exename $loopCount -std > $time_file");
 		unless(open(CODE, $time_file)) {
@@ -262,7 +263,7 @@ sub SumTimes {
 	$baseTime = 0.0;
 	$stdsingle = 0.0;
 	my $ii = 1;
-	#if we're only using real times, don't bother with the corner-cases
+	# if we're only using real times, don't bother with the corner-cases
 	if($realtimes) {
 		$ii = 8;
 	}
@@ -272,7 +273,7 @@ sub SumTimes {
 			$time += CheckTime();
 			$baseTime += $stdsingle;
 		} else {
-			#tests with higher levels of randomness are given higher priority in timing results
+			# tests with higher levels of randomness are given higher priority in timing results
 			print STDOUT "trying $ii $ii\n" if $debug;
 			$time += 2 * $ii * CheckTime();
 			$baseTime += 2 * $ii * $stdsingle;
@@ -302,7 +303,8 @@ sub TuneVariable {
 	for ($$tunevar = $beginval; $$tunevar <= $endval; $$tunevar++) {
 		WriteConstants();
 		$sumtime = SumTimes();
-		#If this value is better, use it.  If this is the start value and it's just as good, use the startval
+		# If this value is better, use it.  If this is the start value
+                # and it's just as good, use the startval
 		if(not $besttime or ($sumtime < $besttime) or (($besttime == $sumtime) and ($$tunevar == $startval))) {
 			$besttime = $sumtime;
 			$best_val = $$tunevar;
