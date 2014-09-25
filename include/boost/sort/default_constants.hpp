5ae57b646161b783b0fd32cb726c6d1764a4ccc2
Steven Ross <spreadsort@gmail.com>
2014-09-24 21:39:42 -0400

    80 character cleanup outside detail

--- include/boost/sort/default_constants.hpp
+++ include/boost/sort/default_constants.hpp
@@ -1,4 +1,4 @@
-//constant definitions for the Sort library
+//constant definitions for the Boost Sort library
 
 //          Copyright Steven J. Ross 2001 - 2014
 // Distributed under the Boost Software License, Version 1.0.
@@ -24,16 +24,16 @@ LOG_MEAN_BIN_SIZE = 2,
 //Used to force a comparison-based sorting for small bins, if it's faster.
 //Minimum value 1
 LOG_MIN_SPLIT_COUNT = 9,
-//This is minimum split count to use spreadsort when it will finish in one iteration
-//make this larger the faster std::sort is relative to integer_sort
+//This is the minimum split count to use spreadsort when it will finish in one
+//iteration.  Make this larger the faster std::sort is relative to integer_sort.
 LOG_FINISHING_COUNT = 31,
 //Sets the minimum number of items per bin for floating point.
 FLOAT_LOG_MEAN_BIN_SIZE = 2,
 //Used to force a comparison-based sorting for small bins, if it's faster.
 //Minimum value 1
 FLOAT_LOG_MIN_SPLIT_COUNT = 8,
-//This is minimum split count to use spreadsort when it will finish in one iteration
-//make this larger the faster std::sort is relative to float_sort
+//This is the minimum split count to use spreadsort when it will finish in one
+//iteration.  Make this larger the faster std::sort is relative to float_sort.
 FLOAT_LOG_FINISHING_COUNT = 4,
 //There is a minimum size below which it is not worth using spreadsort
 MIN_SORT_SIZE = 3000 };
--- tune.pl
+++ tune.pl
@@ -1,4 +1,11 @@
 #!/usr/bin/perl -w
+#          Copyright Steven J. Ross 2008 - 2014
+# Distributed under the Boost Software License, Version 1.0.
+#    (See accompanying file LICENSE_1_0.txt or copy at
+#          http://www.boost.org/LICENSE_1_0.txt)
+#
+# A speed and accuracy testing and automated parameter tuning script.
+
 $usage = "usage: tune.pl [-tune] [-real] [-tune_verify] [-verbose] [-multiple_iterations] [-large] [-small] [-windows] [fileSize]\n";
 # testing sorting on 40 million elements by default
 # don't test on below 2^22 (4 million) elements as that is the minimum
@@ -102,15 +109,15 @@ if($tune) {
                 TuneVariable(\$float_log_min_size, $float_log_mean_bin_size + 1, $max_splits + $float_log_mean_bin_size);
                 print STDOUT "Tuning float_log_mean_bin_size\n";
                 TuneVariable(\$float_log_mean_bin_size, 0, $float_log_min_size - 1);
-		print STDOUT "Tuning float_log_finishing_size\n";
-	    TuneVariable(\$float_log_finishing_count, 1, $float_log_min_size);
-		$exename = "spreadsort";
-	}
+                print STDOUT "Tuning float_log_finishing_size\n";
+            TuneVariable(\$float_log_finishing_count, 1, $float_log_min_size);
+                $exename = "spreadsort";
+        }
 
-	# After optimizations for large datasets are complete, see how small of a dataset can be sped up
-	print STDOUT "Tuning minimum sorting size\n";
-	TuneMinSize();
-	print STDOUT "Writing results\n";
+        # After optimizations for large datasets are complete, see how small of a dataset can be sped up
+        print STDOUT "Tuning minimum sorting size\n";
+        TuneMinSize();
+        print STDOUT "Writing results\n";
 }
 
 # Doing a final run with final settings to compare sort times
@@ -153,18 +160,19 @@ system("rm -f input.txt $log2");
 
 $ENV{'PATH'} = $prev_path;
 
-
+# A simple speed test comparing std::sort to 
 sub PerfTest {
-	my ($message, $local_exe) = @_;
-	$exename = $local_exe;
-	print STDOUT "$message\n";
-	$lastTime = SumTimes();
-	print STDOUT "runtime: $lastTime\n";
-	print STDOUT "std::sort time: $baseTime\n";
-	$speedup = (($baseTime/$lastTime) - 1) * 100;
-	print STDOUT "speedup: ".sprintf("%.2f", $speedup)."%\n";
+        my ($message, $local_exe) = @_;
+        $exename = $local_exe;
+        print STDOUT "$message\n";
+        $lastTime = SumTimes();
+        print STDOUT "runtime: $lastTime\n";
+        print STDOUT "std::sort time: $baseTime\n";
+        $speedup = (($baseTime/$lastTime) - 1) * 100;
+        print STDOUT "speedup: ".sprintf("%.2f", $speedup)."%\n";
 }
 
+# Write an updated constants file as part of tuning.
 sub WriteConstants {
     # deleting the file
     $const_file = 'include/boost/sort/constants.hpp';
@@ -176,158 +184,164 @@ sub WriteConstants {
       print STDERR "Can't open output file: $const_file: $!\n";
       exit;
     }
-	print CONSTANTS "//constant definitions for the Sort library\n\n";
-	print CONSTANTS "//          Copyright Steven J. Ross 2001 - 2014\n";
-	print CONSTANTS "// Distributed under the Boost Software License, Version 1.0.\n";
-	print CONSTANTS "//    (See accompanying file LICENSE_1_0.txt or copy at\n";
-	print CONSTANTS "//          http://www.boost.org/LICENSE_1_0.txt)\n\n";
-	print CONSTANTS "//  See http://www.boost.org/ for updates, documentation, and revision history.\n";
-	print CONSTANTS "#ifndef BOOST_SORT_CONSTANTS\n";
-	print CONSTANTS "#define BOOST_SORT_CONSTANTS\n";
-	print CONSTANTS "namespace boost {\n";
-	print CONSTANTS "namespace detail {\n";
-	print CONSTANTS "//Tuning constants\n";
-	print CONSTANTS "//This should be tuned to your processor cache;\n"; 
+        print CONSTANTS "//constant definitions for the Boost Sort library\n\n";
+        print CONSTANTS "//          Copyright Steven J. Ross 2001 - 2014\n";
+        print CONSTANTS "// Distributed under the Boost Software License, Version 1.0.\n";
+        print CONSTANTS "//    (See accompanying file LICENSE_1_0.txt or copy at\n";
+        print CONSTANTS "//          http://www.boost.org/LICENSE_1_0.txt)\n\n";
+        print CONSTANTS "//  See http://www.boost.org/ for updates, documentation, and revision history.\n";
+        print CONSTANTS "#ifndef BOOST_SORT_CONSTANTS\n";
+        print CONSTANTS "#define BOOST_SORT_CONSTANTS\n";
+        print CONSTANTS "namespace boost {\n";
+        print CONSTANTS "namespace detail {\n";
+        print CONSTANTS "//Tuning constants\n";
+        print CONSTANTS "//This should be tuned to your processor cache;\n"; 
     print CONSTANTS "//if you go too large you get cache misses on bins\n";
-	print CONSTANTS "//The smaller this number, the less worst-case memory usage.\n";  
-	print CONSTANTS "//If too small, too many recursions slow down spreadsort\n";
-	print CONSTANTS "enum { MAX_SPLITS = $max_splits,\n";
-	print CONSTANTS "//It's better to have a few cache misses and finish sorting\n";
-	print CONSTANTS "//than to run another iteration\n";
-	print CONSTANTS "MAX_FINISHING_SPLITS = MAX_SPLITS + 1,\n";
-	print CONSTANTS	"//Sets the minimum number of items per bin.\n";
-	print CONSTANTS "LOG_MEAN_BIN_SIZE = $log_mean_bin_size,\n";
-	print CONSTANTS "//Used to force a comparison-based sorting for small bins, if it's faster.\n";
-	print CONSTANTS "//Minimum value 1\n";
-	$log_min_split_count = $log_min_size - $log_mean_bin_size;
-	print CONSTANTS "LOG_MIN_SPLIT_COUNT = $log_min_split_count,\n";
-	print CONSTANTS "//This is minimum split count to use spreadsort when it will finish in one iteration\n";
-	print CONSTANTS "//make this larger the faster std::sort is relative to integer_sort\n";
+        print CONSTANTS "//The smaller this number, the less worst-case memory usage.\n";  
+        print CONSTANTS "//If too small, too many recursions slow down spreadsort\n";
+        print CONSTANTS "enum { MAX_SPLITS = $max_splits,\n";
+        print CONSTANTS "//It's better to have a few cache misses and finish sorting\n";
+        print CONSTANTS "//than to run another iteration\n";
+        print CONSTANTS "MAX_FINISHING_SPLITS = MAX_SPLITS + 1,\n";
+        print CONSTANTS "//Sets the minimum number of items per bin.\n";
+        print CONSTANTS "LOG_MEAN_BIN_SIZE = $log_mean_bin_size,\n";
+        print CONSTANTS "//Used to force a comparison-based sorting for small bins, if it's faster.\n";
+        print CONSTANTS "//Minimum value 1\n";
+        $log_min_split_count = $log_min_size - $log_mean_bin_size;
+        print CONSTANTS "LOG_MIN_SPLIT_COUNT = $log_min_split_count,\n";
+        print CONSTANTS "//This is the minimum split count to use spreadsort when it will finish in one\n";
+        print CONSTANTS "//iteration  Make this larger the faster std::sort is relative to integer_sort.\n";
     print CONSTANTS "LOG_FINISHING_COUNT = $log_finishing_count,\n";
-	print CONSTANTS	"//Sets the minimum number of items per bin for floating point.\n";
-	print CONSTANTS "FLOAT_LOG_MEAN_BIN_SIZE = $float_log_mean_bin_size,\n";
-	print CONSTANTS "//Used to force a comparison-based sorting for small bins, if it's faster.\n";
-	print CONSTANTS "//Minimum value 1\n";
-	$float_log_min_split_count = $float_log_min_size - $float_log_mean_bin_size;
-	print CONSTANTS "FLOAT_LOG_MIN_SPLIT_COUNT = $float_log_min_split_count,\n";
-	print CONSTANTS "//This is minimum split count to use spreadsort when it will finish in one iteration\n";
-	print CONSTANTS "//make this larger the faster std::sort is relative to float_sort\n";
+        print CONSTANTS "//Sets the minimum number of items per bin for floating point.\n";
+        print CONSTANTS "FLOAT_LOG_MEAN_BIN_SIZE = $float_log_mean_bin_size,\n";
+        print CONSTANTS "//Used to force a comparison-based sorting for small bins, if it's faster.\n";
+        print CONSTANTS "//Minimum value 1\n";
+        $float_log_min_split_count = $float_log_min_size - $float_log_mean_bin_size;
+        print CONSTANTS "FLOAT_LOG_MIN_SPLIT_COUNT = $float_log_min_split_count,\n";
+        print CONSTANTS "//This is the minimum split count to use spreadsort when it will finish in one\n";
+        print CONSTANTS "//iteration.  Make this larger the faster std::sort is relative to float_sort.\n";
     print CONSTANTS "FLOAT_LOG_FINISHING_COUNT = $float_log_finishing_count,\n";
-	print CONSTANTS "//There is a minimum size below which it is not worth using spreadsort\n";
-	print CONSTANTS "MIN_SORT_SIZE = $min_sort_size };\n";
-	print CONSTANTS "}\n}\n#endif\n";
-	close CONSTANTS;
-	system("$makename $exename $log");
+        print CONSTANTS "//There is a minimum size below which it is not worth using spreadsort\n";
+        print CONSTANTS "MIN_SORT_SIZE = $min_sort_size };\n";
+        print CONSTANTS "}\n}\n#endif\n";
+        close CONSTANTS;
+        system("$makename $exename $log");
 }
 
+# Sort the file with both std::sort and boost::sort, verify the results are the
+# same, update stdtime with std::sort time, and return boost::sort time.
 sub CheckTime {
-	my $sort_time = 0.0;
-	my $time_file = "time.txt";
-	# use the line below on systems that can't overwrite.
-	#system("rm -f $time_file");
-	system("$exename $loopCount $std > $time_file");
-	unless(open(CODE, $time_file)) {
-		print STDERR "Could not open file: $time_file: $!\n";
-		exit;
-	}
-	while($line = <CODE>) {
-	 @parts = split("time", $line);
-	 if(@parts > 1) {
-		$sort_time = $parts[1];
-		last;
-	 }		
-	}
-	close(CODE);
-	# verifying correctness
-	if(not $std and $verifycorrect) {
-		system("$exename $loopCount -std > $time_file");
-		unless(open(CODE, $time_file)) {
-			print STDERR "Could not open file: $time_file: $!\n";
-			exit;
-		}
-		die "Difference in results\n" unless (compare("boost_sort_out.txt","standard_sort_out.txt") == 0) ;
-		while($line = <CODE>) {
-	 		@parts = split("time", $line);
-	 		if(@parts > 1) {
-				$stdsingle = $parts[1];
-				last;
-	 		}		
-		}
-		close(CODE);
-	}
-	return $sort_time;
+        my $sort_time = 0.0;
+        my $time_file = "time.txt";
+        # use the line below on systems that can't overwrite.
+        #system("rm -f $time_file");
+        system("$exename $loopCount $std > $time_file");
+        unless(open(CODE, $time_file)) {
+                print STDERR "Could not open file: $time_file: $!\n";
+                exit;
+        }
+        while($line = <CODE>) {
+         @parts = split("time", $line);
+         if(@parts > 1) {
+                $sort_time = $parts[1];
+                last;
+         }              
+        }
+        close(CODE);
+        # verifying correctness
+        if(not $std and $verifycorrect) {
+                system("$exename $loopCount -std > $time_file");
+                unless(open(CODE, $time_file)) {
+                        print STDERR "Could not open file: $time_file: $!\n";
+                        exit;
+                }
+                die "Difference in results\n" unless (compare("boost_sort_out.txt","standard_sort_out.txt") == 0) ;
+                while($line = <CODE>) {
+                        @parts = split("time", $line);
+                        if(@parts > 1) {
+                                $stdsingle = $parts[1];
+                                last;
+                        }               
+                }
+                close(CODE);
+        }
+        return $sort_time;
 }
 
+# Sum up times for different data distributions.  If realtimes is not set, 
+# larger ranges are given a larger weight.
 sub SumTimes {
-	my $time = 0;
-	$baseTime = 0.0;
-	$stdsingle = 0.0;
-	my $ii = 1;
-	# if we're only using real times, don't bother with the corner-cases
-	if($realtimes) {
-		$ii = 8;
-	}
-	for (; $ii <= 16; $ii++) {
-		system("randomgen $ii $ii $fileSize");
-		if($realtimes) {
-			$time += CheckTime();
-			$baseTime += $stdsingle;
-		} else {
-			# tests with higher levels of randomness are given higher priority in timing results
-			print STDOUT "trying $ii $ii\n" if $debug;
-			$time += 2 * $ii * CheckTime();
-			$baseTime += 2 * $ii * $stdsingle;
-			if($ii > 1) {
-				print STDOUT "trying 1 $ii\n" if $debug;
-				system("randomgen 1 $ii $fileSize");
-				$time += $ii * CheckTime();
-				$baseTime += $ii * $stdsingle;
-				print STDOUT "trying $ii 1\n" if $debug;
-				system("randomgen $ii 1 $fileSize");
-				$time += $ii * CheckTime();
-				$baseTime += $ii * $stdsingle;
-			}
-		}
-	}
-	if($time == 0.0) {
-		$time = 0.01;
-	}
-	return $time;
+        my $time = 0;
+        $baseTime = 0.0;
+        $stdsingle = 0.0;
+        my $ii = 1;
+        # if we're only using real times, don't bother with the corner-cases
+        if($realtimes) {
+                $ii = 8;
+        }
+        for (; $ii <= 16; $ii++) {
+                system("randomgen $ii $ii $fileSize");
+                if($realtimes) {
+                        $time += CheckTime();
+                        $baseTime += $stdsingle;
+                } else {
+                        # tests with higher levels of randomness are given higher priority in timing results
+                        print STDOUT "trying $ii $ii\n" if $debug;
+                        $time += 2 * $ii * CheckTime();
+                        $baseTime += 2 * $ii * $stdsingle;
+                        if($ii > 1) {
+                                print STDOUT "trying 1 $ii\n" if $debug;
+                                system("randomgen 1 $ii $fileSize");
+                                $time += $ii * CheckTime();
+                                $baseTime += $ii * $stdsingle;
+                                print STDOUT "trying $ii 1\n" if $debug;
+                                system("randomgen $ii 1 $fileSize");
+                                $time += $ii * CheckTime();
+                                $baseTime += $ii * $stdsingle;
+                        }
+                }
+        }
+        if($time == 0.0) {
+                $time = 0.01;
+        }
+        return $time;
 }
 
+# Tests a range of potential values for a variable, and sets it to the fastest.
 sub TuneVariable {
-	my ($tunevar, $beginval, $endval) = @_;
-	my $best_val = $$tunevar;
-	my $besttime = 0;
-	my $startval = $$tunevar;
-	for ($$tunevar = $beginval; $$tunevar <= $endval; $$tunevar++) {
-		WriteConstants();
-		$sumtime = SumTimes();
-		# If this value is better, use it.  If this is the start value
+        my ($tunevar, $beginval, $endval) = @_;
+        my $best_val = $$tunevar;
+        my $besttime = 0;
+        my $startval = $$tunevar;
+        for ($$tunevar = $beginval; $$tunevar <= $endval; $$tunevar++) {
+                WriteConstants();
+                $sumtime = SumTimes();
+                # If this value is better, use it.  If this is the start value
                 # and it's just as good, use the startval
-		if(not $besttime or ($sumtime < $besttime) or (($besttime == $sumtime) and ($$tunevar == $startval))) {
-			$besttime = $sumtime;
-			$best_val = $$tunevar;
-		}
-		print STDOUT "Value: $$tunevar Time: $sumtime\n" if $verbose;
-	}
-	$$tunevar = $best_val;
-	print STDOUT "Best Value: $best_val\n";
-	if($best_val != $startval) {
-		$changed = 1;
-	}
+                if(not $besttime or ($sumtime < $besttime) or (($besttime == $sumtime) and ($$tunevar == $startval))) {
+                        $besttime = $sumtime;
+                        $best_val = $$tunevar;
+                }
+                print STDOUT "Value: $$tunevar Time: $sumtime\n" if $verbose;
+        }
+        $$tunevar = $best_val;
+        print STDOUT "Best Value: $best_val\n";
+        if($best_val != $startval) {
+                $changed = 1;
+        }
 }
 
+# Determine the cutoff size below which std::sort is faster.
 sub TuneMinSize {
-	for (; $min_sort_size <= $defFileSize; $min_sort_size *= 2) {
-		$loopCount = ($defFileSize/$min_sort_size)/10;
-		$fileSize = $min_sort_size;
-		WriteConstants();
-		$std = "";
-		$sumtime = SumTimes();
-		$std = "-std";
-		$stdtime = SumTimes();
-		print STDOUT "Size: $min_sort_size spreadsort Time: $sumtime std::sort Time: $stdtime\n";
-		last if($stdtime > $sumtime);
-	}
+        for (; $min_sort_size <= $defFileSize; $min_sort_size *= 2) {
+                $loopCount = ($defFileSize/$min_sort_size)/10;
+                $fileSize = $min_sort_size;
+                WriteConstants();
+                $std = "";
+                $sumtime = SumTimes();
+                $std = "-std";
+                $stdtime = SumTimes();
+                print STDOUT "Size: $min_sort_size boost::sort Time: $sumtime std::sort Time: $stdtime\n";
+                last if($stdtime > $sumtime);
+        }
 }
