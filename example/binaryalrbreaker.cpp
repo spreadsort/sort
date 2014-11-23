48d3b7a82d0dbc77ef81d73960b23d046d3069e6
Steven Ross <spreadsort@gmail.com>
2014-11-23 06:27:10 -0500

    Running binaryalrbreaker 10 passes to reduce noise, and swapping 10% of the input to break up reverse-sorted order.

--- example/binaryalrbreaker.cpp
+++ example/binaryalrbreaker.cpp
@@ -44,9 +44,9 @@ fill_vector(vector<DATA_TYPE> & input, const DATA_TYPE base_value,
   }
   else {
     unsigned shift = indices[index];
-    fill_vector(input, (base_value << shift) + ((1 << shift) - 1), 
+    fill_vector(input, (base_value << shift) + ((1 << shift) - 1),
                 remaining_bits - shift, indices, index - 1);
-    fill_vector(input, base_value << shift, remaining_bits - shift, indices, 
+    fill_vector(input, base_value << shift, remaining_bits - shift, indices,
                 index - 1);
   }
 }
@@ -63,41 +63,46 @@ get_index(unsigned count)
 
 //Tests std::sort vs boost::sort on boost::sort's worst distribution.
 int main(int, const char **) {
-  vector<DATA_TYPE> input;
-  vector<unsigned> offsets;
   unsigned total_length = sizeof(DATA_TYPE) * 8;
-  unsigned bit_length = total_length;
-  unsigned bit_offset = bit_shift;
-  bit_length -= radix_threshold;
-  for (; bit_length >= ++bit_offset; bit_length -= bit_offset)
-    offsets.push_back(bit_offset);
-  for (int ii = (1 << bit_length) - 1; ii >= 0; --ii)
-    fill_vector(input, ii, total_length - bit_length,
-                offsets, offsets.size() - 1);
-  printf("size: %lu\n", input.size());
+  double std_sort_time = 0;
+  double spreadsort_time = 0;
+  for (int repetition = 0; repetition < 10; ++repetition) {
+    vector<DATA_TYPE> input;
+    vector<unsigned> offsets;
+    unsigned bit_length = total_length - radix_threshold;
+    unsigned bit_offset = bit_shift;
+    for (; bit_length >= ++bit_offset; bit_length -= bit_offset)
+      offsets.push_back(bit_offset);
+    for (int ii = (1 << bit_length) - 1; ii >= 0; --ii)
+      fill_vector(input, ii, total_length - bit_length,
+                  offsets, offsets.size() - 1);
 
-  //Randomize the inputs slightly so they aren't in reverse-sorted order, for
-  //which std::sort is very fast.
-  for (unsigned u = 0; u < input.size() / 10; ++u)
-    std::swap(input[get_index(input.size())], input[get_index(input.size())]);
+    //Randomize the inputs slightly so they aren't in reverse-sorted order, for
+    //which std::sort is very fast.
+    for (unsigned u = 0; u < input.size() / 10; ++u)
+      std::swap(input[get_index(input.size())], input[get_index(input.size())]);
 
-  //Run both std::sort and boost::sort.
-  for (unsigned u = 0; u < 2; ++u) {
-    vector<DATA_TYPE> array = input;
-    clock_t start, end;
-    double elapsed;
-    start = clock();
-    if (u)
-      std::sort(array.begin(), array.end());
-    else
-      boost::sort(array.begin(), array.end());
-    end = clock();
-    elapsed = static_cast<double>(end - start);
-    if (u)
-      printf("std::sort elapsed time %f\n", elapsed / CLOCKS_PER_SEC);
-    else
-      printf("spreadsort elapsed time %f\n", elapsed / CLOCKS_PER_SEC);
-    array.clear();
+    //Run both std::sort and boost::sort.
+    for (unsigned u = 0; u < 2; ++u) {
+      vector<DATA_TYPE> array = input;
+      clock_t start, end;
+      double elapsed;
+      start = clock();
+      if (u)
+        std::sort(array.begin(), array.end());
+      else
+        boost::sort(array.begin(), array.end());
+      end = clock();
+      elapsed = static_cast<double>(end - start);
+      if (u)
+        std_sort_time += elapsed / CLOCKS_PER_SEC;
+      else
+        spreadsort_time += elapsed / CLOCKS_PER_SEC;
+      array.clear();
+    }
   }
+
+  printf("std::sort elapsed time %f\n", std_sort_time);
+  printf("spreadsort elapsed time %f\n", spreadsort_time);
   return 0;
 }
