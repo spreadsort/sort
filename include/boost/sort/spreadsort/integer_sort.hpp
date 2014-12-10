f1f1ae7a81531bb46e7489337e72fcf8cff3b07d
Steven Ross <spreadsort@gmail.com>
2014-12-09 22:46:28 -0500

    Adding new files as part of library restructuring

---
+++ include/boost/sort/spreadsort/integer_sort.hpp
@@ -0,0 +1,60 @@
+//Templated Spreadsort-based implementation of integer_sort
+
+//          Copyright Steven J. Ross 2001 - 2014.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+// See http://www.boost.org/libs/sort/ for library home page.
+      
+/*
+Some improvements suggested by:
+Phil Endecott and Frank Gennari
+*/
+
+#ifndef BOOST_INTEGER_SORT_HPP
+#define BOOST_INTEGER_SORT_HPP
+#include <algorithm>
+#include <vector>
+#include <cstring>
+#include <limits>
+#include <boost/static_assert.hpp>
+#include <boost/sort/spreadsort/detail/constants.hpp>
+#include <boost/sort/spreadsort/detail/integer_sort.hpp>
+
+namespace boost {
+namespace sort {
+  //Top-level sorting call for integers
+  template <class RandomAccessIter>
+  inline void integer_sort(RandomAccessIter first, RandomAccessIter last) 
+  {
+    //Don't sort if it's too small to optimize
+    if (last - first < detail::min_sort_size)
+      std::sort(first, last);
+    else
+      detail::integer_sort(first, last, *first >> 0);
+  }
+
+  //integer_sort with functors
+  template <class RandomAccessIter, class Right_shift, class Compare>
+  inline void integer_sort(RandomAccessIter first, RandomAccessIter last,
+                           Right_shift shift, Compare comp) {
+    if (last - first < detail::min_sort_size)
+      std::sort(first, last, comp);
+    else
+      detail::integer_sort(first, last, shift(*first, 0), shift, comp);
+  }
+
+  //integer_sort with Right_shift functor
+  template <class RandomAccessIter, class Right_shift>
+  inline void integer_sort(RandomAccessIter first, RandomAccessIter last,
+                           Right_shift shift) {
+    if (last - first < detail::min_sort_size)
+      std::sort(first, last);
+    else
+      detail::integer_sort(first, last, shift(*first, 0), shift);
+  }
+}
+}
+
+#endif
---
+++ include/boost/sort/spreadsort/spreadsort.hpp
@@ -0,0 +1,67 @@
+// Templated generic hybrid sorting
+
+//          Copyright Steven J. Ross 2001 - 2009.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+// See http://www.boost.org/libs/sort/ for library home page.
+      
+/*
+Some improvements suggested by:
+Phil Endecott and Frank Gennari
+float_mem_cast fix provided by:
+Scott McMurray
+*/
+
+#ifndef BOOST_SORT_SPREADSORT_HPP
+#define BOOST_SORT_SPREADSORT_HPP
+#include <algorithm>
+#include <vector>
+#include <cstring>
+#include <string>
+#include <limits>
+#include <boost/type_traits.hpp>
+#include <boost/sort/spreadsort/integer_sort.hpp>
+#include <boost/sort/spreadsort/float_sort.hpp>
+#include <boost/sort/spreadsort/string_sort.hpp>
+
+namespace boost {
+namespace sort {
+  //Generic spreadsort call to integer_sort
+  template <class RandomAccessIter>
+  inline typename boost::enable_if_c< std::numeric_limits< 
+    typename std::iterator_traits<RandomAccessIter>::value_type >::is_integer, 
+    void >::type
+  spreadsort(RandomAccessIter first, RandomAccessIter last) 
+  {
+    integer_sort(first, last);
+  }
+
+  //Generic spreadsort call to float_sort
+  template <class RandomAccessIter>
+  inline typename boost::enable_if_c< !std::numeric_limits< 
+    typename std::iterator_traits<RandomAccessIter>::value_type >::is_integer
+    && std::numeric_limits< 
+    typename std::iterator_traits<RandomAccessIter>::value_type >::is_iec559, 
+    void >::type
+  spreadsort(RandomAccessIter first, RandomAccessIter last) 
+  {
+    float_sort(first, last);
+  }
+
+  //Generic spreadsort call to string_sort for strings and wstrings
+  template <class RandomAccessIter>
+  inline typename boost::enable_if_c< 
+    is_same<typename std::iterator_traits<RandomAccessIter>::value_type, 
+            typename std::string>::value || 
+    is_same<typename std::iterator_traits<RandomAccessIter>::value_type, 
+            typename std::wstring>::value, void >::type
+  spreadsort(RandomAccessIter first, RandomAccessIter last)
+  {
+    string_sort(first, last);
+  }
+}
+}
+
+#endif
